0a10ee3655646a390007a6edda90ab3e270bf6e5
Jacky Tay <jacky@smudgeapps.com>
2017-07-10 16:23:55 +1200

    Initiate project

---
+++ UtilityHelper/UtilityHelper/UtilityHelper.h
@@ -0,0 +1,19 @@
+//
+//  UtilityHelper.h
+//  UtilityHelper
+//
+//  Created by Jacky Tay on 10/07/17.
+//  Copyright © 2017 JackyTay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UtilityHelper.
+FOUNDATION_EXPORT double UtilityHelperVersionNumber;
+
+//! Project version string for UtilityHelper.
+FOUNDATION_EXPORT const unsigned char UtilityHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilityHelper/PublicHeader.h>
+
+
